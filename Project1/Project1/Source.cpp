cc5f7c7e8ba4a2f2a90abb0fe6b172fd98c61a3b
Peter Aziz <peter-aziz@aucegypt.edu>
2023-10-22 20:33:34 +0200

    added VS project files

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+//1. [10 Points] Read in (and validate) a Boolean function given as Assume that the variable name is a single Latin letter starting from “a”.Think about the checkers needed to validate the inputand list them in your report.
+
+//2.[10 Points] Print the truth table of the function as well as the canonical SoP and PoS.
+
+//3.[20 Points] Generate and print all prime implicants(PIs).For each PI show the minterms it covers as well as its binary representation.
+
+//4.[15 Points] Using the PIs generated in part 2, obtainand print all the essential prime implicants EPIs(as Boolean expressions).
+
+//5.[5 Pts] Print the minterms that are not covered by the essential PIs.
+
+//6.[10 Points, Bonus] Solve the PI table and print the minimized Boolean expression of the Boolean function using the 3 - step heuristic discussed in the lecture.
+
+//7.[5 Pts] Draw the K - Map showing the solution(up to 4 variables)
+
+//8.[5 Pts] Draw the logic circuit of the minimized function(up to 10 variables); you may use https ://wavedrom.com/tutorial2.html
+
+int main() {
+
+	return 0;
+
+}
